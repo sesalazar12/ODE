3e47e7242da111b4b9f9cd770fd21376441483b2
sesalazar12 <sebastian.salazarchaves@ucr.ac.cr>
2024-06-25 17:49:50 -0600

    anadir directorio ode, docs y mkdocs

---
+++ ode/__init__.py
@@ -0,0 +1,10 @@
+"""Se muestran ciertos metodos de resolucion de Ecuaciones Diferenciales Ordinarias con condicion inicial.
+
+El modulo contiene:
+
+-'euler(x0,t,f)' - Devuelve un arreglo con la solucion utilizando el metodo de Euler.
+-'rk2(x0,t,f)' - Devuelve un arreglo con la solucion utilizando el metodo Runge-Kutta de 4to orden.
+-'rk4(x0,t,f)' - Devuelve un arreglo con la solucion utilizando el metodo Runge-
+Kutta de 4to orden.
+
+"""
